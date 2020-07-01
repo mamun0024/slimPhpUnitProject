1ca9c55fe23f35080f7700bc074a91520ec2b3ef
MD. Abdullah Al Mamun <abdullah.mamun@bs-23.net>
2020-07-02 02:06:37 +0600

    Update Container interfade path.

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -2,21 +2,21 @@
 
 namespace App\Controllers;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 
 abstract class Controller
 {
     /**
      * The container instance.
      *
-     * @var \Interop\Container\ContainerInterface
+     * @var ContainerInterface
      */
     protected $c;
 
     /**
      * Set up controllers to have access to the container.
      *
-     * @param \Interop\Container\ContainerInterface $container
+     * @param ContainerInterface $container
      */
     public function __construct(ContainerInterface $container)
     {
