d0d3791970c716ef074035df94848413c8c17305
MD. Abdullah Al Mamun <abdullah.mamun@bs-23.net>
2020-07-04 01:38:53 +0600

    Configure Test Environment in PhpStorm & run first test.

---
+++ test/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace test;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testBehavior(): void
+    {
+        $this->assertTrue(true);
+    }
+}
