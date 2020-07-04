426cf68b537b5f532658c9c3d6b6b654b097d8cd
MD. Abdullah Al Mamun <abdullah.mamun@bs-23.net>
2020-07-05 01:20:42 +0600

    Implement authentication machanism on route.

--- app/Middleware/AuthenticateMiddleware.php
+++ app/Middleware/AuthenticateMiddleware.php
@@ -2,7 +2,60 @@
 
 namespace App\Middleware;
 
+use App\Utils\Traits\ResponseTrait;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 class AuthenticateMiddleware
 {
+    use ResponseTrait;
 
+    /**
+     * @var array
+     */
+    private $settings;
+
+    /**
+     * Get application settings data.
+     *
+     * @param array $settings
+     */
+    public function __construct(array $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    /**
+     * AuthenticateService middleware invokable class
+     *
+     * @param ServerRequestInterface $request  PSR7 request
+     * @param ResponseInterface $response PSR7 response
+     * @param callable $next     Next middleware
+     *
+     * @return ResponseInterface
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next)
+    {
+        if ($this->passes($request)) {
+            return $next($request, $response);
+        }
+
+        $this->response_status  = false;
+        $this->response_code    = 401;
+        $this->response_message = "Unauthorized !!!";
+        $this->response_data    = null;
+        $this->response_details = null;
+        return $response->withStatus($this->response_code)->withJson($this->responseData());
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return mixed
+     */
+    private function passes(ServerRequestInterface $request)
+    {
+        // Implement authentication machanism here.
+        return true;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use App\Controllers\HomeController;
 use App\Controllers\UserController;
+use App\Middleware\AuthenticateMiddleware;
 
 define("CREATE_UPDATE_FUNCTION", 'createOrUpdate');
 
 $app->get('/', HomeController::class . ':index');
 
-$app->group('/users', function () {
-    $this->post('', UserController::class . ':'.CREATE_UPDATE_FUNCTION);
-    $this->put('/{id}', UserController::class . ':'.CREATE_UPDATE_FUNCTION);
-});
+// Authenticated API's
+$app->group('/api/v1', function () {
+    $this->group('/users', function () {
+        $this->post('', UserController::class . ':'.CREATE_UPDATE_FUNCTION);
+        $this->put('/{id}', UserController::class . ':'.CREATE_UPDATE_FUNCTION);
+    });
+})->add(new AuthenticateMiddleware($container->get('settings')->all()));
