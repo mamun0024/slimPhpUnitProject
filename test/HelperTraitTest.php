c787480cc1726a8bc62c42315edce08a0873dd11
MD. Abdullah Al Mamun <abdullah.mamun@bs-23.net>
2020-07-05 02:32:12 +0600

    Helper Trait PHP unit test - DONE.

---
+++ test/HelperTraitTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace test;
+
+use App\Utils\Traits\HelperTrait;
+use PHPUnit\Framework\TestCase;
+
+class HelperTraitTest extends TestCase
+{
+    use HelperTrait;
+
+    public function testCheckWithNull(): void
+    {
+        $this->assertFalse($this->emptyCheck(null));
+    }
+
+    public function testCheckWithEmptyValue(): void
+    {
+        $this->assertFalse($this->emptyCheck(''));
+    }
+
+    public function testCheckWithIsset(): void
+    {
+        $value = null;
+        $this->assertFalse($this->emptyCheck($value));
+    }
+}
